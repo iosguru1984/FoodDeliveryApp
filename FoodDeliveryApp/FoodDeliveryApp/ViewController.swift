974cc95615d56470acc919a3db3c6b08aa9dfda7
baiv84 <65267292+baiv84@users.noreply.github.com>
2024-02-12 01:13:12 +0700

    Added new fonts

--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.text = "Hello world!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 400),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20),
+        ])
+        
+        label.font = .systemFont(ofSize: 40, weight: UIFont.Weight(rawValue: 20))
+        label.textColor = .white
+
+
         view.backgroundColor = .green
     }
 
