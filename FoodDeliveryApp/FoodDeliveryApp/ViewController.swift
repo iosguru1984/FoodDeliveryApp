6805ff2788ea5a89719350a11b097ba1ff250a3e
baiv84 <65267292+baiv84@users.noreply.github.com>
2024-02-12 00:22:22 +0700

    Create Base project, removed storyboard

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Ivan Babintsev on 11.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
